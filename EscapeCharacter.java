92024c3d151d5be736c7fd8645ad065a772e9c47
jaceuche <nnukwuuchenna2008@gmail.com>
2026-05-07 16:11:09 +0100

    JavaApplicationSeptMMS2025

---
+++ EscapeCharacter.java
@@ -0,0 +1,10 @@
+public class EscapeCharacter{
+	public static void main(String[] args){
+		System.out.print("when you need to use the escape characters \n");
+		System.out.print("use it when you want to manipulate your cusor\n");
+		System.out.print("use \\n when you want a newline\n");
+		System.out.print("who is the father of java?. The father of java is \"James Goaling\"n");
+		System.out.print("we are using carrage return here");
+	}
+		
+}
---
+++ PrimitiveDataType,java.java
@@ -0,0 +1,6 @@
+public class PrimitiveDataType{
+	public static void main(String[] args){
+		byte myAge = 30;
+		System.out.print("Miss mercy is %d years old",myage);
+	}
+}
