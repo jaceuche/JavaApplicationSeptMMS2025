2898f7e765e18cf6967290a8fafeb9ae90021b27
jaceuche <nnukwuuchenna2008@gmail.com>
2026-05-14 12:41:00 +0100

    JavaApplicationSeptMMS2025

---
+++ TypeCasting.java
@@ -0,0 +1,8 @@
+public class TypeCasting{
+	public static void main(String [] args){
+		double price = 7658;
+		System.out.printf("The price of laptop is %f%n",price);
+		
+		double balance = 5693.875;
+	}
+} 
